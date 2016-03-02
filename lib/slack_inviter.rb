39a60ecabf09f7c31df697ce247ebb094e378f87
Diego Silva <diego.silva@live.com>
2016-03-02 20:33:49 -0300

    adding invite functionality

--- lib/slack_inviter.rb
+++ lib/slack_inviter.rb
@@ -1,5 +1,27 @@
-require "slack_inviter/version"
+require 'slack_inviter/version'
+require 'net/http'
+require 'json'
 
 module SlackInviter
-  # Your code goes here...
+  class Configuration
+    attr_accessor :team, :token
+  end
+
+  class SlackInviterError < StandardError
+  end
+
+  def self.invite(email)
+    uri = URI.parse "https://#{@configuration.team}.slack.com/api/users.admin.invite"
+
+    api_response = Net::HTTP.post_form(uri, email: email, token: @configuration.token)
+    response = JSON.parse(api_response.body)
+
+    raise SlackInviterError, response['error'] unless response['ok']
+    response['ok']
+  end
+
+  def self.configure
+    @configuration ||= Configuration.new
+    yield @configuration
+  end
 end
--- spec/slack_inviter_spec.rb
+++ spec/slack_inviter_spec.rb
@@ -5,7 +5,46 @@ describe SlackInviter do
     expect(SlackInviter::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  before :each do
+    SlackInviter.configure do |config|
+      config.team = 'exampleteam'
+      config.token = 'TOKEN'
+    end
+  end
+
+  it 'does allow configuration' do
+    config = SlackInviter.instance_variable_get('@configuration')
+    expect(config.team).to eq 'exampleteam'
+    expect(config.token).to eq 'TOKEN'
+  end
+
+  it 'does invite user' do
+    expect(Net::HTTP).to receive(:post_form).with(URI.parse('https://exampleteam.slack.com/api/users.admin.invite'),
+                                                  email: 'me@example.com',
+                                                  token: 'TOKEN').and_return(double(body: '{"ok":true}'))
+
+    expect(SlackInviter.invite('me@example.com')).to be_truthy
+  end
+
+  context 'when ok is false it does raise exception' do
+    it 'fail when user already in team' do
+      allow(Net::HTTP).to receive(:post_form).and_return(double(body: '{"ok":false,"error":"already_in_team"}'))
+      expect { SlackInviter.invite('me@example.com') }.to raise_error SlackInviter::SlackInviterError, 'already_in_team'
+    end
+
+    it 'fail when user alreay invited' do
+      allow(Net::HTTP).to receive(:post_form).and_return(double(body: '{"ok":false,"error":"already_invited"}'))
+      expect { SlackInviter.invite('me@example.com') }.to raise_error SlackInviter::SlackInviterError, 'already_invited'
+    end
+
+    it 'fail when email is invalid' do
+      allow(Net::HTTP).to receive(:post_form).and_return(double(body: '{"ok":false,"error":"invalid_email"}'))
+      expect { SlackInviter.invite('me@example.com') }.to raise_error SlackInviter::SlackInviterError, 'invalid_email'
+    end
+
+    it 'fail when token is invalid' do
+      allow(Net::HTTP).to receive(:post_form).and_return(double(body: '{"ok":false,"error":"invalid_auth"}'))
+      expect { SlackInviter.invite('me@example.com') }.to raise_error SlackInviter::SlackInviterError, 'invalid_auth'
+    end
   end
 end
